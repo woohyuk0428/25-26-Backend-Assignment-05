9b3cddb8fba53db857a7c5dec60f479c7c4620cb
woohyuk0428 <whkang03@gmail.com>
2025-11-13 23:31:26 +0900

    feat : GoogleLoginService 구현

---
+++ 강우혁/TodoList/src/main/java/com/gdg/todolist/service/GoogleLoginService.java
@@ -0,0 +1,115 @@
+package com.gdg.todolist.service;
+
+import com.gdg.todolist.domain.Provider;
+import com.gdg.todolist.domain.Role;
+import com.gdg.todolist.domain.User;
+import com.gdg.todolist.dto.TokenDto;
+import com.gdg.todolist.dto.UserInfoDto;
+import com.gdg.todolist.exception.BadReqeustException;
+import com.gdg.todolist.exception.UserNotFoundException;
+import com.gdg.todolist.jwt.TokenProvider;
+import com.gdg.todolist.repository.UserRepository;
+import com.google.gson.Gson;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.security.Principal;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class GoogleLoginService {
+
+    private final String GOOGLE_TOKEN_URL = "https://www.googleapis.com/token";
+    @Value("jwt.google-client-id")
+    private String GOOGLE_CLIENT_ID;
+    @Value("jwt.google-client-pw")
+    private String GOOGLE_CLIENT_PW;
+    private final String GOOGLE_REDIRECT_URI = "http://localhost:8080/api/callback/google";
+
+    private final UserRepository userRepository;
+    private final TokenProvider tokenProvider;
+
+    public String getGoogleAccessToken(String code) {
+        RestTemplate restTemplate = new RestTemplate();
+        Map<String, String> params = Map.of(
+                "code", code,
+                "scope", "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email",
+                "client_id", GOOGLE_CLIENT_ID,
+                "client_secret", GOOGLE_CLIENT_PW,
+                "redirect_uri", GOOGLE_REDIRECT_URI,
+                "grant_type", "authorization_code"
+        );
+
+        ResponseEntity<String> responseEntity = restTemplate.postForEntity(GOOGLE_TOKEN_URL, params, String.class);
+
+        if (responseEntity.getStatusCode().is2xxSuccessful()) {
+            String json = responseEntity.getBody();
+            Gson gson = new Gson();
+
+            return gson.fromJson(json, TokenDto.class)
+                    .getAccessToken();
+        }
+
+        throw new BadReqeustException("구글 엑세스 토큰을 가져오는데 실패했습니다.");
+    }
+
+    public TokenDto loginOrSingUp(String getGoogleAccessToken) {
+        UserInfoDto userInfoDto = getUserInfo(getGoogleAccessToken);
+
+        if(!userInfoDto.getVerifiedEmail()){
+            throw new UserNotFoundException("유저 정보가 없습니다.");
+        }
+
+        User user = userRepository.findByEmail(userInfoDto.getEmail())
+                .orElseGet(() -> userRepository.save(User.builder()
+                        .email(userInfoDto.getEmail())
+                        .name(userInfoDto.getName())
+                        .pictureUrl(userInfoDto.getPictureUrl())
+                        .role(Role.ROLE_USER)
+                        .provider(Provider.GOOGLE)
+                        .build()
+                ));
+
+        return TokenDto.builder()
+                .accessToken(tokenProvider.createAccessToken(user))
+                .refreshToken(tokenProvider.createRefreshToken(user))
+                .build();
+    }
+
+    private UserInfoDto getUserInfo(String accessToken) {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "https://www.googleapis.com/oauth2/v2/userinfo?access_token=" + accessToken;
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + accessToken);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        RequestEntity<Void> requestEntity = new RequestEntity<>(headers, HttpMethod.GET, URI.create(url));
+        ResponseEntity<String> responseEntity = restTemplate.exchange(requestEntity, String.class);
+
+        if (responseEntity.getStatusCode().is2xxSuccessful()) {
+            String json = responseEntity.getBody();
+            Gson gson = new Gson();
+            return gson.fromJson(json, UserInfoDto.class);
+        }
+
+        throw new UserNotFoundException("유저 정보를 가져오는데 실패했습니다.");
+    }
+
+    public User test(Principal principal) {
+        Long id = Long.parseLong(principal.getName());
+
+        return userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException("유저를 찾을 수 없습니다."));
+    }
+
+}
