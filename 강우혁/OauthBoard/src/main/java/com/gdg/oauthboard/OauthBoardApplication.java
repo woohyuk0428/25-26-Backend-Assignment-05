3cf05a1d057916fa220b7f6a145f0c11e43f7972
woohyuk0428 <whkang03@gmail.com>
2025-11-13 15:54:07 +0900

    feat: application.yml 환경 변수 초기설정

---
+++ 강우혁/OauthBoard/src/main/java/com/gdg/oauthboard/OauthBoardApplication.java
@@ -0,0 +1,13 @@
+package com.gdg.oauthboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthBoardApplication.class, args);
+    }
+
+}
---
+++ 강우혁/OauthBoard/src/test/java/com/gdg/oauthboard/OauthBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdg.oauthboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
