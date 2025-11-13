e2dd4a8fbd868f0ccdd3d55bf0843266838dbf83
woohyuk0428 <whkang03@gmail.com>
2025-11-13 23:51:21 +0900

    feat : UserSignUp 구현

--- 강우혁/TodoList/src/main/java/com/gdg/todolist/dto/UserSignUp.java
+++ 강우혁/TodoList/src/main/java/com/gdg/todolist/dto/UserSignUp.java
@@ -3,7 +3,7 @@ package com.gdg.todolist.dto;
 import lombok.Getter;
 
 @Getter
-public class UserSingUp {
+public class UserSignUp {
     private String name;
     private String email;
     private String password;
